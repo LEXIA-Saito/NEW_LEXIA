451986f8816a521cf799f683c128451b2cea50e9
Saito <95484621+LEXIA-Saito@users.noreply.github.com>
2025-07-17 09:42:43 +0900

    update Lexia logo urls

--- lib/config.ts
+++ lib/config.ts
@@ -2,6 +2,6 @@ export const SITE_URL =
   process.env.NEXT_PUBLIC_SITE_URL || "https://lexia.design";
 
 export const LOGO_URL =
-  "https://2iuxfx58zw36rxwq.public.blob.vercel-storage.com/logo/lexia_logo.webp";
+  "https://2iuxfx58zw36rxwq.public.blob.vercel-storage.com/logo/new_lexia_black_logo.svg";
 export const LOGO_WHITE_URL =
-  "https://2iuxfx58zw36rxwq.public.blob.vercel-storage.com/logo/lexia_logo_white.webp";
+  "https://2iuxfx58zw36rxwq.public.blob.vercel-storage.com/logo/new_lexia_white_logo.svg";
