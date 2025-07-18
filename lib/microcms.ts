d76b19b0df3f1ae538bb73868ed840cc638ad547
Saito <95484621+LEXIA-Saito@users.noreply.github.com>
2025-07-18 14:04:22 +0900

    連携コミット

--- lib/microcms.ts
+++ lib/microcms.ts
@@ -1,8 +1,8 @@
 import { createClient } from 'microcms-js-sdk'
 
 export const microcmsClient = createClient({
-  serviceDomain: process.env.NEXT_PUBLIC_MICROCMS_DOMAIN || '',
-  apiKey: process.env.MICROCMS_API_KEY || '',
+  serviceDomain: process.env.LEXIA_MICROCMS_SERVICE_DOMAIN || '',
+  apiKey: process.env.LEXIA_MICROCMS_API_KEY || '',
 })
 
 export const getPosts = async () => {
