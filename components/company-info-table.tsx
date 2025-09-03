89bc68665d0081de2932d1a998d442687dc2c351
Saito <95484621+LEXIA-Saito@users.noreply.github.com>
2025-09-03 12:25:25 +0900

    fix: PC表示で事業情報が折り返される問題を修正

--- components/company-info-table.tsx
+++ components/company-info-table.tsx
@@ -7,23 +7,23 @@ const CompanyInfoTable: FC = () => {
     <table className="w-full text-sm text-neutral-700 dark:text-neutral-300">
       <tbody>
         <tr>
-          <th className="py-2 pr-4 text-left font-semibold text-neutral-900 dark:text-neutral-100 align-top">事業名</th>
-          <td className="py-2">LEXIA（レキシア）</td>
+          <th className="py-2 pr-4 text-left font-semibold text-neutral-900 dark:text-neutral-100 align-top md:whitespace-nowrap">事業名</th>
+          <td className="py-2 md:whitespace-nowrap">LEXIA（レキシア）</td>
         </tr>
         <tr>
-          <th className="py-2 pr-4 text-left font-semibold text-neutral-900 dark:text-neutral-100 align-top">所在地</th>
-          <td className="py-2">愛知県碧南市川端町1-45</td>
+          <th className="py-2 pr-4 text-left font-semibold text-neutral-900 dark:text-neutral-100 align-top md:whitespace-nowrap">所在地</th>
+          <td className="py-2 md:whitespace-nowrap">愛知県碧南市川端町1-45</td>
         </tr>
         <tr>
-          <th className="py-2 pr-4 text-left font-semibold text-neutral-900 dark:text-neutral-100 align-top">設立</th>
-          <td className="py-2">2022年3月26日</td>
+          <th className="py-2 pr-4 text-left font-semibold text-neutral-900 dark:text-neutral-100 align-top md:whitespace-nowrap">設立</th>
+          <td className="py-2 md:whitespace-nowrap">2022年3月26日</td>
         </tr>
         <tr>
-          <th className="py-2 pr-4 text-left font-semibold text-neutral-900 dark:text-neutral-100 align-top">代表者</th>
-          <td className="py-2">齋藤 雅人</td>
+          <th className="py-2 pr-4 text-left font-semibold text-neutral-900 dark:text-neutral-100 align-top md:whitespace-nowrap">代表者</th>
+          <td className="py-2 md:whitespace-nowrap">齋藤 雅人</td>
         </tr>
         <tr>
-          <th className="py-2 pr-4 text-left font-semibold text-neutral-900 dark:text-neutral-100 align-top">事業内容</th>
+          <th className="py-2 pr-4 text-left font-semibold text-neutral-900 dark:text-neutral-100 align-top md:whitespace-nowrap">事業内容</th>
           <td className="py-2">
             <ul className="list-disc pl-5 space-y-1">
               <li>WEB制作（コーポレートサイト・ECサイト・ポートフォリオサイト・ランディングページ・採用サイト）</li>
@@ -33,12 +33,12 @@ const CompanyInfoTable: FC = () => {
           </td>
         </tr>
         <tr>
-          <th className="py-2 pr-4 text-left font-semibold text-neutral-900 dark:text-neutral-100 align-top">電話番号</th>
-          <td className="py-2">090-1742-3456</td>
+          <th className="py-2 pr-4 text-left font-semibold text-neutral-900 dark:text-neutral-100 align-top md:whitespace-nowrap">電話番号</th>
+          <td className="py-2 md:whitespace-nowrap">090-1742-3456</td>
         </tr>
         <tr>
-          <th className="py-2 pr-4 text-left font-semibold text-neutral-900 dark:text-neutral-100 align-top">メール</th>
-          <td className="py-2">lexia0web@gmail.com</td>
+          <th className="py-2 pr-4 text-left font-semibold text-neutral-900 dark:text-neutral-100 align-top md:whitespace-nowrap">メール</th>
+          <td className="py-2 md:whitespace-nowrap">lexia0web@gmail.com</td>
         </tr>
       </tbody>
     </table>
