21e124ecb18f747a30d3696241aeed70a7a2c16d
Saito <95484621+LEXIA-Saito@users.noreply.github.com>
2025-09-22 10:26:15 +0900

    Allow Google Maps iframe

--- next.config.mjs
+++ next.config.mjs
@@ -54,7 +54,8 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://vercel.live https://www.googletagmanager.com https://www.google-analytics.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https: blob:; connect-src 'self' https://api.resend.com https://vercel.live https://www.google-analytics.com; frame-ancestors 'none';",
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://vercel.live https://www.googletagmanager.com https://www.google-analytics.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https: blob:; connect-src 'self' https://api.resend.com https://vercel.live https://www.google-analytics.com; frame-src 'self' https://www.google.com https://maps.google.com; frame-ancestors 'none';",
           },
         ],
       },
